60484ea76bd0174f6a12d3cb26430718876e2979
Dustin Lee <dlee35@gmail.com>
2019-08-27 20:18:00 -0400

    various updates for vmware

--- Vagrant/scripts/securityonion-rename-interfaces.sh
+++ Vagrant/scripts/securityonion-rename-interfaces.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 echo "Renaming interfaces for consistency"
 for NIC in $(ip link | grep -E 'ens|eth[0-9]{1,}|enp'| cut -d\: -f2 ); do
+    # VMware Workstation naming
     if [ "$NIC" == "ens192"  ]; then
         echo "SUBSYSTEM==\"net\", ACTION==\"add\", ATTR{address}==\"$(cat /sys/class/net/$NIC/address)\", NAME=\"vagrant0\"" > /etc/udev/rules.d/70-persistent-net.rules
     elif [ "$NIC" == "ens32"  ]; then
         echo "SUBSYSTEM==\"net\", ACTION==\"add\", ATTR{address}==\"$(cat /sys/class/net/$NIC/address)\", NAME=\"ens33\"" >> /etc/udev/rules.d/70-persistent-net.rules
     elif [ "$NIC" == "ens33"  ]; then
         echo "SUBSYSTEM==\"net\", ACTION==\"add\", ATTR{address}==\"$(cat /sys/class/net/$NIC/address)\", NAME=\"ens34\"" >> /etc/udev/rules.d/70-persistent-net.rules
-    else
-        echo "Improperly named interface not found"
+    # VirtualBox naming
+    elif [ "$NIC" == "enp0s3"  ]; then
+        echo "SUBSYSTEM==\"net\", ACTION==\"add\", ATTR{address}==\"$(cat /sys/class/net/$NIC/address)\", NAME=\"vagrant0\"" > /etc/udev/rules.d/70-persistent-net.rules
+    elif [ "$NIC" == "enp0s8"  ]; then
+        echo "SUBSYSTEM==\"net\", ACTION==\"add\", ATTR{address}==\"$(cat /sys/class/net/$NIC/address)\", NAME=\"ens33\"" >> /etc/udev/rules.d/70-persistent-net.rules
+    elif [ "$NIC" == "enp0s9"  ]; then
+        echo "SUBSYSTEM==\"net\", ACTION==\"add\", ATTR{address}==\"$(cat /sys/class/net/$NIC/address)\", NAME=\"ens34\"" >> /etc/udev/rules.d/70-persistent-net.rules
     fi
 done
